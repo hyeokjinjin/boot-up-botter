b4695042fd3394d6905a229244ab07bb67fcb54a
Hyeokjin Jin <hyeokjinjin@gmail.com>
2023-08-08 15:46:07 -0500

    Update

--- Main.py
+++ Main.py
@@ -1,39 +1,96 @@
-import responses
+from typing import Optional
 import discord
-
-async def send_message(message, user_message):
-    try:
-        response = responses.handle_response(user_message)
-        await message.channel.send(response)
-        
-    except Exception as e:
-        print(e)
+import time
+from discord.ext import commands
+from discord.ext.commands import MemberConverter
+from apikey import *
+import asyncio
 
 
-def runBot():
-    TOKEN = 'MTEzODI4MDg4MTgwMzUwOTc2MA.GN-8HT.aVVyGKQiF4lXi9JkWYis_j4mDAakXlbei_NLZc'
-    client = discord.Client(intents = discord.Intents.all())   
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='$', intents=intents)
 
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running')
+
+@client.event
+async def on_ready():
+    print("Bot has been booted up")
+    print("----------------------")
     
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+
+
+@client.command()
+async def shutdown(ctx):
+    await ctx.send("Shutting Down")
+    await client.close()
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello. I'm booted up!")
+
+
+@client.command()
+async def everyone(ctx):
+    for guild in client.guilds:
+        for user in guild.members:
+            if user != None:                
+                await ctx.send(f'<@{user.id}>')
+
+
+@client.command()
+async def mention(ctx, *, user: discord.User=None):
+    if user != None:        
+        for i in range(10):
+            await ctx.send(f'<@{user.id}>')
+            time.sleep(1)
+            await ctx.send("Boot up")
+            time.sleep(1.25)
+    else:
+        await ctx.send('User not found. Try again.')
+
+
+@client.command()
+async def bootup(ctx, *, user: discord.User=None):
+    if user != None:
+        victim = client.get_user(user.id)
+        for i in range(10):
+            await victim.send("Boot up")
+            await ctx.send(f'<@{user.id}>')
+            time.sleep(1)
+            await ctx.send("Boot up")
+            time.sleep(1.25)
+    else:
+        await ctx.send('User not found. Try again.')
         
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+
+@client.command()
+async def bootupPlan(ctx, *args):
+    #try:
+    print(f"Booting up {args[0]} in {args[1]} {args[2]}")
+    if args[2] == "hours":
+        seconds = (int(args[1]) * 3600)
+    elif args[2] == "minutes":
+        seconds = (int(args[1]) * 60)
+    elif args[2] == "seconds":
+        seconds = (int(args[1]))
         
-        print(f"{username} said: '{user_message}' in {channel}")
-        
-        if user_message[0] == '$':
-            user_message = user_message[1:]
-            await send_message(message, user_message)
+    await ctx.send(f"Booting up {args[0]} in {args[1]} {args[2]}.")
     
+    await asyncio.sleep(seconds) 
         
-    client.run(TOKEN)
-    
-runBot()
+    converter = MemberConverter()
+    user = await converter.convert(ctx, args[0])
+        
+    if user != None:
+        for i in range(10):
+            await ctx.send(f'<@{user.id}>')
+            time.sleep(1)
+            await ctx.send("Boot up")
+            time.sleep(1.25)
+    else:
+        await ctx.send('User not found. Try again.')
+
+    #except:
+        #await ctx.send('No time specified. Try again with format "$bootupPlan {user} {time} {unit of seconds}."')
+
+client.run(BOTTOKEN)
--- responses.py
+++
@@ -1,7 +0,0 @@
-
-
-def handle_response(message) -> str:
-    p_message = message.lower()
-    
-    if p_message == "$bootup":
-        return "What time do you want to boot everyone up? (Respond with a time with starting with \"$$\")"
