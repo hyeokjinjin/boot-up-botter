52f1bfe2b6aa94db2ef002604819ac46b0da0e5c
Hyeokjin Jin <hyeokjinjin@gmail.com>
2023-08-10 23:34:08 -0500

    Added voice functionality

--- Main.py
+++ Main.py
@@ -84,5 +84,22 @@ async def bootupLater(ctx, *args):
         await ctx.send('Unknown input. Try again using "$bootupLater (username [multiple]) (time) (plural units).')
 
 
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("User not found in a voice channel. Join a Voice Channel and try again.")
+
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Left voice channel.")
+    else:
+        await ctx.send("Not in a voice channel.")
+
 
 client.run(BOTTOKEN)
